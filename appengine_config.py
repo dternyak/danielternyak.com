71403ab684dec209d811a2fa07471df49a297cad
Daniel Ternyak <dternyak@gmail.com>
2019-09-22 14:59:08 -0500

    new site

--- appengine_config.py
+++
@@ -1,5 +0,0 @@
-"""`appengine_config` gets loaded when starting a new application instance."""
-import sys
-import os.path
-appstats_CALC_RPC_COSTS = True
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
